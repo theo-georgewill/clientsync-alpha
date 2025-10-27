a55764801911fc0e0a4ac43d55e857d65ad95099
Theo <ttggwll@gmail.com>
2025-10-27 15:09:25 +0100

    remove user_id from activities table

--- backend/database/seeders/ActivitySeeder.php
+++ backend/database/seeders/ActivitySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use App\Models\Activity; 
 
 class ActivitySeeder extends Seeder
 {
