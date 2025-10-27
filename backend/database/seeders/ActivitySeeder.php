56dd010a9e1b551771ca37e7c4ecbc005d9d59a7
Theo <ttggwll@gmail.com>
2025-10-27 14:46:09 +0100

    create activity database seeder

---
+++ backend/database/seeders/ActivitySeeder.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ActivitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $activities = [
+            [
+                'user_id' => 1,
+                'hubspot_account_id' => 1,
+                'object_type' => 'contact',
+                'event_type' => 'contact_created',
+                'object_id' => '12345',
+                'title' => 'Contact Created',
+                'description' => 'Jane Doe was added to the CRM.',
+                'occurred_at' => Carbon::parse('2025-06-19 10:30:00'),
+                'details' => json_encode(['name' => 'Jane Doe']),
+            ],
+            [
+                'user_id' => 1,
+                'hubspot_account_id' => 1,
+                'object_type' => 'deal',
+                'event_type' => 'deal_moved',
+                'object_id' => '67890',
+                'title' => 'Deal Moved',
+                'description' => 'CRM Integration deal moved from Proposal to Negotiation.',
+                'occurred_at' => Carbon::parse('2025-06-18 15:15:00'),
+                'details' => json_encode(['from_stage' => 'Proposal', 'to_stage' => 'Negotiation']),
+            ],
+            [
+                'user_id' => 1,
+                'hubspot_account_id' => 1,
+                'object_type' => 'note',
+                'event_type' => 'note_added',
+                'object_id' => '98765',
+                'title' => 'Note Added',
+                'description' => 'Follow-up call scheduled with John Smith.',
+                'occurred_at' => Carbon::parse('2025-06-18 11:00:00'),
+                'details' => json_encode(['subject' => 'Follow-up call']),
+            ],
+            [
+                'user_id' => 1,
+                'hubspot_account_id' => 1,
+                'object_type' => 'integration',
+                'event_type' => 'integration_synced',
+                'object_id' => null,
+                'title' => 'Integration Synced',
+                'description' => 'HubSpot sync completed successfully.',
+                'occurred_at' => Carbon::parse('2025-06-17 09:00:00'),
+                'details' => json_encode(['status' => 'success']),
+            ],
+        ];
+
+        foreach ($activities as $activity) {
+            Activity::create($activity);
+        }
+    }
+}
